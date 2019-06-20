6c1df1a1a9ff8afe01100cd01b45a640c8d9fac3
Lucas Gomes <51804711+gomes0@users.noreply.github.com>
2019-06-20 15:16:37 -0300

    Add files via upload

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -1,8 +1,20 @@
-window.onload = function(){
+window.onload = function () {
     $("#fadeBlackScreen").fadeOut(3000);
 }
 
-function loadGame(){
+var game = [
+    1, 2, 3, 4,
+    5, 6, 7, 8,
+    9, 10, 11, 12,
+    13, 14, 15, 16
+]
+
+function loadGame() {
     $("#main-menu").fadeOut(600);
     $("#gameScreen").fadeIn(600);
+
+
+    for(var i = 0; i<16; i++){
+    $("#squarePos" + game[i - 1]).css("background", "url('media/" + i + ".png')");
+    }
 }
